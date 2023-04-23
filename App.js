4023e9c6406a335d1aac40739cffa6d6adfb5e44
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-04-23 21:25:29 +0300

    Boot splash

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import RNBootSplash from 'react-native-bootsplash';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import MemoryBoard from './src/components/cardMemory/MemoryBoard';
 
 function App() {
+  React.useEffect(() => {
+    RNBootSplash.hide({fade: true, duration: 300});
+  });
+
   return (
     <SafeAreaProvider>
       <MemoryBoard />
--- ios/Brainy/AppDelegate.mm
+++ ios/Brainy/AppDelegate.mm
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
-
+#import "RNBootSplash.h"
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
@@ -10,6 +10,9 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  
+  UIView *rootView = self.window.rootViewController.view; //react-native >= 0.71
+    [RNBootSplash initWithStoryboard:@"LaunchScreen" rootView:rootView]; //initialize the splash screen
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
