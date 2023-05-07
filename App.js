e080d1e89851048f18ca3dc226be2938d9ec80fa
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-05-07 14:13:42 +0300

    ignore warning non-seriazable values

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 //Libs
 import React from 'react';
+import {LogBox} from 'react-native';
 import RNBootSplash from 'react-native-bootsplash';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
@@ -7,6 +8,10 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Router from './src/routes/Router';
 
 function App() {
+  LogBox.ignoreLogs([
+    'Non-serializable values were found in the navigation state',
+  ]);
+
   React.useEffect(() => {
     RNBootSplash.hide({fade: true, duration: 300});
   });
