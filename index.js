2f8d5da00399d6301f62d50b7a4b19a9395e31b6
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-05-12 11:17:49 +0300

    Config lib for env file

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import App from './App';
+import Config from 'react-native-config';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 GoogleSignin.configure({
   offlineAccess: true,
-  webClientId:
-    '687899979713-d4v8o9ab6ckto7nbnvicpfuhe3qbakfk.apps.googleusercontent.com',
+  webClientId: Config.GOOGLE_WEB_CLIENT_ID,
   scopes: ['profile', 'email'],
 });
 
