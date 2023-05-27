10758f5759613fcabe9c3ebc63ab24bc266e7564
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-05-27 19:47:37 +0200

    make home screen scrollable

--- src/screens/GamesScreen.js
+++ src/screens/GamesScreen.js
@@ -1,6 +1,13 @@
+import {
+  View,
+  StatusBar,
+  StyleSheet,
+  Animated,
+  Platform,
+  ScrollView,
+} from 'react-native';
 import React from 'react';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import {View, StatusBar, StyleSheet, Animated, Platform} from 'react-native';
 
 import {Colors} from '../utils/Colors';
 import dict from '../assets/values/dict.json';
@@ -28,29 +35,46 @@ const GamesScreen = ({navigation, route}) => {
 
   return (
     <View style={styles.container}>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={Colors.background}
+      />
       <Header insets={insets} label={dict?.gamesScrTitle} />
       <Animated.View style={[styles.gamesContainer, {opacity: opacityRef}]}>
-        <StatusBar
-          barStyle={'light-content'}
-          backgroundColor={Colors.background}
-        />
-        <HomeGameCard
-          onPress={() => navigation.navigate('MemoryCard')}
-          image={require('../assets/images/memory_match.png')}
-          label={dict.memoryCardsGameTitle}
-        />
-        <View style={styles.seperator} />
-        <HomeGameCard
-          onPress={() => navigation.navigate('ColorCard')}
-          image={require('../assets/images/color_match.png')}
-          label={dict.colorMatchGameTitle}
-        />
-        <View style={styles.seperator} />
-        <HomeGameCard
-          onPress={() => navigation.navigate('EqualMath')}
-          image={require('../assets/images/match_equal.png')}
-          label={dict.doTheMathGameTitle}
-        />
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.scrollContainer}
+          style={{
+            marginBottom: DimensionsUtils.getDP(8),
+            marginTop:
+              insets.top > 0
+                ? insets.top + DimensionsUtils.getDP(58)
+                : DimensionsUtils.getDP(76),
+          }}>
+          <HomeGameCard
+            onPress={() => navigation.navigate('MemoryCard')}
+            image={require('../assets/images/memory_match.png')}
+            label={dict.memoryCardsGameTitle}
+          />
+          <View style={styles.seperator} />
+          <HomeGameCard
+            onPress={() => navigation.navigate('ColorCard')}
+            image={require('../assets/images/color_match.png')}
+            label={dict.colorMatchGameTitle}
+          />
+          <View style={styles.seperator} />
+          <HomeGameCard
+            onPress={() => navigation.navigate('EqualMath')}
+            image={require('../assets/images/match_equal.png')}
+            label={dict.doTheMathGameTitle}
+          />
+          <View style={styles.seperator} />
+          <HomeGameCard
+            onPress={() => navigation.navigate('GestureIt')}
+            image={require('../assets/images/gesture_it.png')}
+            label={dict.gestureItGameTitle}
+          />
+        </ScrollView>
       </Animated.View>
     </View>
   );
@@ -61,9 +85,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Colors.background,
   },
+  scrollContainer: {
+    flexGrow: 1,
+  },
   gamesContainer: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: Colors.background,
   },
