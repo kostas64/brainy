e0255ee8734b2e0f8b5022a321367402cb853ada
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-06-15 18:10:06 +0300

    gamesList component

---
+++ src/components/common/GamesList.js
@@ -0,0 +1,229 @@
+import {
+  View,
+  Text,
+  Animated,
+  FlatList,
+  Pressable,
+  Dimensions,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
+import {
+  State,
+  Directions,
+  FlingGestureHandler,
+} from 'react-native-gesture-handler';
+import React from 'react';
+import FastImage from 'react-native-fast-image';
+
+import {Colors} from '../../utils/Colors';
+import dict from '../../assets/values/dict.json';
+import {AuthContext} from '../../context/AuthProvider';
+import {DimensionsUtils} from '../../utils/DimensionUtils';
+
+const {width} = Dimensions.get('window');
+const AnimPressable = Animated.createAnimatedComponent(Pressable);
+
+const VISIBLE_ITEMS = 3;
+const ITEM_WIDTH = width * 0.76;
+const ITEM_HEIGHT = ITEM_WIDTH * 1.7;
+const SPACING = DimensionsUtils.getDP(10);
+const OVERFLOW_HEIGHT = DimensionsUtils.getDP(72);
+
+const OverflowItems = ({data, bestScores, loadingScores, scrollXAnimated}) => {
+  const {user} = React.useContext(AuthContext);
+
+  const inputRange = [-1, 0, 1];
+
+  const translateY = scrollXAnimated.interpolate({
+    inputRange,
+    outputRange: [OVERFLOW_HEIGHT, 0, -OVERFLOW_HEIGHT],
+  });
+
+  return (
+    <View style={styles.overflowContainer}>
+      <Animated.View style={{transform: [{translateY}]}}>
+        {data.map((item, index) => {
+          const ms = bestScores[item.title]?.[0]?.milliseconds;
+          const points = bestScores[item.title]?.[0]?.points;
+          const scoreLabel = `Best: ${`${
+            !!ms ? `${ms} ms` : `${points} points`
+          }`}`;
+
+          return (
+            <View key={index} style={styles.itemContainer}>
+              <Text style={[styles.title]} numberOfLines={1}>
+                {item.title}
+              </Text>
+              <View style={styles.itemContainerRow}>
+                <Text style={[styles.location]}>{item.description}</Text>
+                {user?.isGuest ? null : loadingScores ? (
+                  <ActivityIndicator size={'small'} color={Colors.tabBarIcon} />
+                ) : (
+                  <Text style={[styles.date]}>
+                    {!!ms || !!points ? scoreLabel : dict?.gamesNoScore}
+                  </Text>
+                )}
+              </View>
+            </View>
+          );
+        })}
+      </Animated.View>
+    </View>
+  );
+};
+
+const GamesList = ({
+  data,
+  index,
+  setIndex,
+  bestScores,
+  scrollXIndex,
+  loadingScores,
+  setActiveIndex,
+  scrollXAnimated,
+}) => {
+  const onFlingLeft = e => {
+    if (e.nativeEvent.state === State.END) {
+      if (index === data.length - 1) return;
+
+      setIndex(oldInd => oldInd + 1);
+      scrollXIndex.setValue(index + 1);
+    }
+  };
+
+  const onFlingRight = ev => {
+    if (ev.nativeEvent.state === State.END) {
+      if (index === 0) {
+        return;
+      }
+      setActiveIndex(index - 1);
+    }
+  };
+
+  const cellRenderedComponent = ({item, index, children, style, ...props}) => {
+    const newStyle = [style, {zIndex: data.length - index}];
+    return (
+      <View style={newStyle} index={index} {...props}>
+        {children}
+      </View>
+    );
+  };
+
+  const renderItem = ({item, index}) => {
+    const inputRange = [index - 1, index, index + 1];
+
+    const translateX = scrollXAnimated.interpolate({
+      inputRange,
+      outputRange: [50, 0, -width],
+    });
+
+    const scale = scrollXAnimated.interpolate({
+      inputRange,
+      outputRange: [0.8, 1, 1.3],
+    });
+
+    const opacity = scrollXAnimated.interpolate({
+      inputRange,
+      outputRange: [1 - 2 / VISIBLE_ITEMS, 1, 0],
+    });
+
+    return (
+      <AnimPressable
+        onPress={item?.onPress}
+        style={{
+          position: 'absolute',
+          left: -ITEM_WIDTH / 2,
+          opacity,
+          transform: [
+            {
+              translateX,
+            },
+            {scale},
+          ],
+        }}>
+        <FastImage source={item.poster} style={styles.image} />
+      </AnimPressable>
+    );
+  };
+
+  return (
+    <FlingGestureHandler
+      key="left"
+      direction={Directions.LEFT}
+      onHandlerStateChange={onFlingLeft}>
+      <FlingGestureHandler
+        key="right"
+        direction={Directions.RIGHT}
+        onHandlerStateChange={onFlingRight}>
+        <View style={styles.container}>
+          <OverflowItems
+            data={data}
+            bestScores={bestScores}
+            loadingScores={loadingScores}
+            scrollXAnimated={scrollXAnimated}
+          />
+          <FlatList
+            data={data}
+            inverted
+            horizontal
+            scrollEnabled={false}
+            removeClippedSubviews={false}
+            contentContainerStyle={styles.listContainer}
+            CellRendererComponent={cellRenderedComponent}
+            keyExtractor={(_, index) => String(index)}
+            renderItem={renderItem}
+          />
+        </View>
+      </FlingGestureHandler>
+    </FlingGestureHandler>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: DimensionsUtils.getDP(8),
+  },
+  listContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    paddingVertical: SPACING * 2,
+  },
+  title: {
+    color: Colors.tabBarIcon,
+    textTransform: 'uppercase',
+    fontFamily: 'Poppins-Bold',
+    fontSize: DimensionsUtils.getFontSize(22),
+  },
+  location: {
+    color: Colors.tabBarIcon,
+    fontFamily: 'Poppins-Medium',
+    fontSize: DimensionsUtils.getFontSize(16),
+  },
+  date: {
+    color: Colors.appGreen,
+    fontFamily: 'Poppins-Medium',
+    fontSize: DimensionsUtils.getFontSize(14),
+  },
+  itemContainer: {
+    height: OVERFLOW_HEIGHT,
+    paddingHorizontal: SPACING * 3,
+  },
+  itemContainerRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  overflowContainer: {
+    height: OVERFLOW_HEIGHT,
+    overflow: 'hidden',
+  },
+  image: {
+    width: ITEM_WIDTH,
+    height: ITEM_HEIGHT,
+    borderRadius: DimensionsUtils.getDP(16),
+  },
+});
+
+export default GamesList;
