7d05f4f29609995eb74de3f46634af1c9bb6eb80
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-06-16 15:55:14 +0300

    bugsnag configuration

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ import App from './App';
 import Config from 'react-native-config';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
+import Bugsnag from '@bugsnag/react-native';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
+
+Bugsnag.start();
 
 GoogleSignin.configure({
   offlineAccess: true,
