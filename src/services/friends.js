b186883f9a4b9dd6be0f70bca4d81bdb07586407
Konstantinos Efkarpidis <kostas11062@gmail.com>
2024-02-08 12:15:55 +0200

    friends services for friendship & requests

--- src/services/friends.js
+++ src/services/friends.js
@@ -1,14 +1,42 @@
 import {
   HOST,
   BEARER,
+  FRIENDSHIP,
   FRIENDS_REQUEST,
   CANCEL_FRIENDS_REQUEST,
   ACCEPT_FRIENDS_REQUEST,
+  CHECK_FRIENDS_REQUEST,
 } from '../Endpoints';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+export const areWeFriends = async userId_requested => {
+  console.log('API areWeFriends ', `${HOST}${FRIENDSHIP}`, userId_requested);
+
+  try {
+    const token = await AsyncStorage.getItem('token');
+
+    return fetch(`${HOST}${FRIENDSHIP}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `${BEARER}${token}`,
+      },
+      body: JSON.stringify({
+        userId_requested,
+      }),
+    });
+  } catch (e) {
+    console.log('API areWeFriends failed', e);
+  }
+};
+
 export const sendFriendsRequest = async userId_requested => {
-  console.log('API sendFriendsRequest ', `${HOST}${FRIENDS_REQUEST}`);
+  console.log(
+    'API sendFriendsRequest ',
+    `${HOST}${FRIENDS_REQUEST}`,
+    userId_requested,
+  );
 
   try {
     const token = await AsyncStorage.getItem('token');
@@ -21,10 +49,8 @@ export const sendFriendsRequest = async userId_requested => {
         Authorization: `${BEARER}${token}`,
       },
       body: JSON.stringify({
-        userId_requested: '658063b79c12c6a21206895f',
+        userId_requested,
       }),
-    }).then(async res => {
-      //   const data = await res.json();
     });
   } catch (e) {
     console.log('API sendFriendsRequest failed', e);
@@ -45,10 +71,8 @@ export const cancelFriendsRequest = async userId_requested => {
         Authorization: `${BEARER}${token}`,
       },
       body: JSON.stringify({
-        userId_requested: '658063b79c12c6a21206895f',
+        userId_requested,
       }),
-    }).then(async res => {
-      //   const data = await res.json();
     });
   } catch (e) {
     console.log('API cancelFriendsRequest failed', e);
@@ -71,10 +95,30 @@ export const acceptFriendsRequest = async userId_requested => {
       body: JSON.stringify({
         userId_requested: '658063b79c12c6a21206895f',
       }),
-    }).then(async res => {
-      //   const data = await res.json();
     });
   } catch (e) {
     console.log('API acceptFriendsRequest failed', e);
+  }
+};
+
+export const getFriendsRequest = async userId_requested => {
+  console.log('API getFriendsRequest ', `${HOST}${CHECK_FRIENDS_REQUEST}`);
+
+  try {
+    const token = await AsyncStorage.getItem('token');
+
+    return fetch(`${HOST}${CHECK_FRIENDS_REQUEST}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `${BEARER}${token}`,
+      },
+      body: JSON.stringify({
+        userId_requested,
+      }),
+    });
+  } catch (e) {
+    console.log('API getFriendsRequest failed', e);
   }
 };
