6ecca436daa0d22dc9f582d6db76920f5017d56e
Konstantinos Efkarpidis <kostas11062@gmail.com>
2024-02-16 23:03:43 +0200

    replace days with frequency, copies

--- src/assets/values/days.js
+++
@@ -1,1 +0,0 @@
-export const DAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
---
+++ src/assets/values/frequency.js
@@ -0,0 +1,6 @@
+import {RepeatFrequency} from '@notifee/react-native';
+
+export const FREQUENCY = [
+  {label: 'Daily', freq: RepeatFrequency.DAILY},
+  {label: 'Weekly', freq: RepeatFrequency.WEEKLY},
+];
