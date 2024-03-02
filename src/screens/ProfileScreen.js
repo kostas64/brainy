6a2579a842321230b6248c77d475a22f94afbb90
Konstantinos Efkarpidis <kostas11062@gmail.com>
2024-03-02 17:17:06 +0200

    add api to get bestofuser scores + skeleton

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -3,15 +3,72 @@ import {StyleSheet} from 'react-native';
 
 import dict from '../assets/values/dict.json';
 import Screen from '../components/common/Screen';
+import Skeleton from '../components/skeleton/Skeleton';
+import {DimensionsUtils} from '../utils/DimensionUtils';
+import {getBestOfScoresForUser} from '../services/score';
+import ProfileScoresSection from '../components/profile/ProfileScoresSection';
+import UserProfileModalAvatar from '../components/userProfileModal/UserProfileModalAvatar';
 
-const ProfileScreen = () => {
+const ProfileScreen = ({route}) => {
+  const [scores, setScores] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  const {item} = route?.params;
+
+  const name = item?.user?.[0].nickname
+    ? item?.user?.[0]?.nickname
+    : `${item?.user?.[0]?.name} ${item?.user?.[0]?.surname}`;
+
+  //** ----- EFFECTS -----
+  React.useEffect(() => {
+    getBestOfScoresForUser(item?.user?.[0]?._id)
+      .then(data => setScores(data))
+      .catch(e => console.log('Error ', e))
+      .finally(() => setLoading(false));
+  }, [item?.user]);
+
   return (
     <Screen label={dict.profileScrTitle} noIcon>
-      {/*  */}
+      <Skeleton
+        type={'profile'}
+        loading={loading}
+        skeletonStyle={styles.skeletonStyle}>
+        <UserProfileModalAvatar
+          name={name}
+          contStyle={styles.avatarCont}
+          imgStyle={styles.imgStyle}
+          imgContStyle={styles.imgCont}
+          user={{avatar: item?.user?.[0]?.avatar}}
+        />
+        <ProfileScoresSection passedScores={scores} />
+      </Skeleton>
     </Screen>
   );
 };
 
 export default ProfileScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  skeletonStyle: {
+    marginLeft: DimensionsUtils.getDP(16),
+    marginTop: DimensionsUtils.getDP(16),
+  },
+  avatarCont: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: DimensionsUtils.getDP(8),
+    width: DimensionsUtils.getDP(112),
+    height: DimensionsUtils.getDP(112),
+    borderRadius: DimensionsUtils.getDP(56),
+  },
+  imgCont: {
+    width: DimensionsUtils.getDP(106),
+    height: DimensionsUtils.getDP(106),
+    borderRadius: DimensionsUtils.getDP(53),
+  },
+  imgStyle: {
+    width: DimensionsUtils.getDP(102),
+    height: DimensionsUtils.getDP(102),
+    borderRadius: DimensionsUtils.getDP(51),
+  },
+});
