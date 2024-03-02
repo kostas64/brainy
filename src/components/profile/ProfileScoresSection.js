0abb51f4313443143a08998e4228834e9e317741
Konstantinos Efkarpidis <kostas11062@gmail.com>
2024-03-02 17:16:21 +0200

    pass score data to adapter

--- src/components/profile/ProfileScoresSection.js
+++ src/components/profile/ProfileScoresSection.js
@@ -6,14 +6,14 @@ import {Colors} from '../../utils/Colors';
 import MenuItem from '../common/MenuItem';
 import {signOut} from '../../services/auth';
 import {WIDTH} from '../../utils/GenericUtils';
-import dict from '../../assets/values/dict.json';
 import {useStorage} from '../../hooks/useStorage';
 import {LIST_GAMES} from '../../assets/values/games';
+import {getAdaptedScores} from '../../utils/StringUtils';
 import {useAuthContext} from '../../context/AuthProvider';
 import {DimensionsUtils} from '../../utils/DimensionUtils';
 import {PROFILE_SECTIONS_3} from '../../assets/values/profile';
 
-const ProfileScoresSection = () => {
+const ProfileScoresSection = ({passedScores}) => {
   const navigation = useNavigation();
 
   const [scores] = useStorage('scores', []);
@@ -43,26 +43,17 @@ const ProfileScoresSection = () => {
 
   return (
     <View style={styles.container}>
-      {LIST_GAMES.map((item, index) => {
-        const ms = scores[item.title]?.[0]?.milliseconds;
-        const points = scores[item.title]?.[0]?.points;
-        const scoreLabel = `${`${
-          ms
-            ? `${ms / 1000}s (${scores[item.title]?.[0]?.flips} flips)`
-            : `${points} points (${scores[item.title]?.[0]?.correctness}%)`
-        }`}`;
-        const score = !!ms || !!points ? scoreLabel : dict?.gamesNoScore;
-
-        return (
-          <View key={`game-${index}`} style={styles.itemContainer}>
-            <View style={styles.iconContainer}>{item.icon}</View>
-            <Text style={styles.label} numberOfLines={2}>
-              {item.title}
-            </Text>
-            <Text style={styles.score}>{score}</Text>
-          </View>
-        );
-      })}
+      {LIST_GAMES.map((item, index) => (
+        <View key={`game-${index}`} style={styles.itemContainer}>
+          <View style={styles.iconContainer}>{item.icon}</View>
+          <Text style={styles.label} numberOfLines={2}>
+            {item.title}
+          </Text>
+          <Text style={styles.score}>
+            {getAdaptedScores(item.title, scores, passedScores)}
+          </Text>
+        </View>
+      ))}
     </View>
   );
 };
--- src/utils/StringUtils.js
+++ src/utils/StringUtils.js
@@ -1,3 +1,30 @@
+import dict from '../assets/values/dict.json';
+
 export const capFirstLet = string => {
   return `${string?.charAt(0)?.toUpperCase()}${string?.slice(1)}`;
+};
+
+export const getAdaptedScores = (game, scores, passedScores) => {
+  if (passedScores) {
+    const item = passedScores?.filter(gm => gm.game === game)?.[0]?.data?.[0];
+    const ms = item?.milliseconds;
+    const points = item?.points;
+    const scoreLabel = `${`${
+      ms
+        ? `${ms / 1000}s (${item?.flips} flips)`
+        : `${points} points (${item?.correctness}%)`
+    }`}`;
+    const score = !!ms || !!points ? scoreLabel : dict?.gamesNoScore;
+    return score;
+  } else {
+    const ms = scores[game]?.[0]?.milliseconds;
+    const points = scores[game]?.[0]?.points;
+    const scoreLabel = `${`${
+      ms
+        ? `${ms / 1000}s (${scores[game]?.[0]?.flips} flips)`
+        : `${points} points (${scores[game]?.[0]?.correctness}%)`
+    }`}`;
+    const score = !!ms || !!points ? scoreLabel : dict?.gamesNoScore;
+    return score;
+  }
 };
