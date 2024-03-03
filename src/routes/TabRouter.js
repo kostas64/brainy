4571132fcee0b72488616753e3322b0a7de8b996
Konstantinos Efkarpidis <kostas11062@gmail.com>
2024-03-04 00:38:28 +0200

    nada

--- src/routes/TabRouter.js
+++ src/routes/TabRouter.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import MyProfile from '../screens/MyProfile';
 import RankScreen from '../screens/RankScreen';
 import TabBar from '../components/common/TabBar';
 import GamesScreen from '../screens/GamesScreen';
+import MyProfile from '../screens/MyProfileScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,11 +12,7 @@ const Tabs = props => <TabBar {...props} />;
 
 const TabStack = () => {
   return (
-    <Tab.Navigator
-      tabBar={Tabs}
-      screenOptions={() => ({
-        headerShown: false,
-      })}>
+    <Tab.Navigator tabBar={Tabs} screenOptions={{headerShown: false}}>
       <Tab.Screen name="Games" component={GamesScreen} />
       <Tab.Screen name="Rank" component={RankScreen} />
       <Tab.Screen name="MyProfile" component={MyProfile} />
