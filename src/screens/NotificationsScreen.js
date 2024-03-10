62c554ff40a89254cf3505db983f43388b735a0b
Konstantinos Efkarpidis <kostas11062@gmail.com>
2024-03-10 02:32:03 +0200

    handle reminder notification in notificaitons screen for ios & android

--- src/screens/NotificationsScreen.js
+++ src/screens/NotificationsScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Text, View, Linking, StyleSheet} from 'react-native';
+import {useAnimatedStyle, useSharedValue} from 'react-native-reanimated';
+import notifee, {AndroidNotificationSetting} from '@notifee/react-native';
 
 import {Colors} from '../utils/Colors';
 import dict from '../assets/values/dict.json';
@@ -7,11 +9,14 @@ import {useStorage} from '../hooks/useStorage';
 import Screen from '../components/common/Screen';
 import {useAppState} from '../hooks/useAppState';
 import {updateNotifSetts} from '../services/user';
+import {animateInput} from '../utils/AnimateUtils';
 import InfoBox from '../components/common/InfoBox';
 import {useInteraction} from '../hooks/useInteraction';
 import {DimensionsUtils} from '../utils/DimensionUtils';
 import {hasNotPermissions} from '../utils/PermissionUtils';
 import CustomSwitch from '../components/common/CustomSwitch';
+import {NotificationUtils} from '../utils/NotificationsUtils';
+import {isAndroid, isTiramisuAndHigher} from '../utils/GenericUtils';
 
 const NotificationItem = ({value, setValue, title, caption}) => {
   return (
@@ -28,8 +33,11 @@ const NotificationItem = ({value, setValue, title, caption}) => {
 };
 
 const NotificationsScreen = () => {
+  const translateX = useSharedValue(0);
+
   const firstRender = React.useRef(0);
   const [showBox, setShowBox] = React.useState(false);
+  const [boxLabel, setBoxLabel] = React.useState(null);
 
   const [settings, setSettings] = useStorage('notifSets', {
     score: true,
@@ -37,19 +45,78 @@ const NotificationsScreen = () => {
     reminder: true,
   });
 
+  const isSwitchDisabled = boxLabel === dict.infoAlarmLabel && isAndroid;
+
+  //** ----- STYLES -----
+  const animStyle = useAnimatedStyle(
+    () => ({transform: [{translateX: translateX.value}]}),
+    [],
+  );
+
   //** ----- FUNCTIONS -----
-  const onPressInfoBox = () => {
-    Linking.openSettings();
+  const onPressInfoBox = async () => {
+    if (boxLabel === dict.infoAlarmLabel) {
+      await notifee.openAlarmPermissionSettings();
+    } else {
+      Linking.openSettings();
+    }
+  };
+
+  const handleReminderNotification = React.useCallback(value => {
+    notifee.getTriggerNotificationIds().then(async ids => {
+      const idToDelete = ids.find(id => id === 'reminder');
+
+      if (!value && idToDelete) {
+        await notifee.cancelTriggerNotification(idToDelete);
+      } else if (value) {
+        const {trigger, payload} = NotificationUtils.getReminderPayload();
+        await notifee.createTriggerNotification(payload, trigger);
+      }
+    });
+  }, []);
+
+  const onToggleReminder = () => {
+    if (isSwitchDisabled) {
+      animateInput(translateX);
+    } else {
+      let value;
+
+      setSettings(old => {
+        value = !old.reminder;
+        return {...old, reminder: !old.reminder};
+      });
+
+      handleReminderNotification(value);
+    }
   };
 
   const checkForPerms = React.useCallback(async () => {
-    const res = await hasNotPermissions();
-    setShowBox(!res);
-  }, []);
+    if (isAndroid) {
+      const alarm = await notifee.getNotificationSettings();
+
+      if (
+        isTiramisuAndHigher &&
+        alarm.android.alarm !== AndroidNotificationSetting.ENABLED
+      ) {
+        //Case is Android  >= Tiramisu && Alarm disabled
+        setSettings(old => ({...old, reminder: false}));
+        setBoxLabel(dict.infoAlarmLabel);
+        !showBox && setShowBox(true);
+      } else if (isTiramisuAndHigher) {
+        //Case is Android >= Tiramisu && Alarm enabled
+        showBox && setShowBox(false);
+        setBoxLabel(null);
+        setSettings(old => ({...old, reminder: true}));
+      }
+    } else {
+      const res = await hasNotPermissions();
+      setBoxLabel(dict.infoNotLabel);
+      setShowBox(!res);
+    }
+  }, [showBox, setSettings]);
 
   //** ----- EFFECTS -----
   useAppState(checkForPerms, false);
-
   useInteraction(checkForPerms);
 
   React.useEffect(() => {
@@ -79,22 +146,17 @@ const NotificationsScreen = () => {
         />
         <NotificationItem
           value={settings.reminder}
-          setValue={() =>
-            setSettings(old => ({
-              ...old,
-              reminder: !old.reminder,
-            }))
-          }
+          setValue={onToggleReminder}
           title={dict.notifyReminderTitle}
           caption={dict.notifyReminderSub}
         />
 
         <InfoBox
           showBox={showBox}
+          infoLabel={boxLabel}
           onPress={onPressInfoBox}
-          infoLabel={dict.infoNotLabel}
-          containerStyle={styles.infoBoxContainer}
           callToActionLabel={dict.infoNotCtaLabel}
+          containerStyle={[styles.infoBoxContainer, animStyle]}
         />
       </View>
     </Screen>
