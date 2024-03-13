58b58f3557f0a15ec0f3da8f45990c48934de601
Konstantinos Efkarpidis <kostas11062@gmail.com>
2024-03-13 09:55:05 +0200

    remove unused file

--- src/assets/values/frequency.js
+++
@@ -1,6 +0,0 @@
-import {RepeatFrequency} from '@notifee/react-native';
-
-export const FREQUENCY = [
-  {label: 'Daily', freq: RepeatFrequency.DAILY},
-  {label: 'Weekly', freq: RepeatFrequency.WEEKLY},
-];
