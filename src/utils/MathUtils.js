0742971fa22dd01801a303ebc35bdfbc3df3560d
Konstantinos Efkarpidis <kostas11062@gmail.com>
2024-03-15 10:26:31 +0200

    remove moment and date-picker , replace moment logic with Date js

--- src/utils/MathUtils.js
+++ src/utils/MathUtils.js
@@ -1,5 +1,3 @@
-import moment from 'moment';
-
 export const shuffleArray = array => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -19,36 +17,34 @@ export const getRandom = (min, max) => {
 };
 
 export const calcTimestamp = timestamp => {
-  const ms = new Date().getTime() - new Date(timestamp);
-  const duration = moment.duration(ms);
+  const currentTime = new Date().getTime();
+  const targetTime = new Date(timestamp).getTime();
+  const msDifference = currentTime - targetTime;
 
-  const years = Math.floor(duration.asYears());
-  duration.subtract(years, 'years');
+  const msInHour = 3600000;
+  const msInDay = 86400000;
+  const msInWeek = 604800000;
+  const msInMonth = 2629746000;
+  const msInYear = 31556952000;
 
-  const months = Math.floor(duration.asMonths());
-  duration.subtract(months, 'months');
+  const years = Math.floor(msDifference / msInYear);
+  const months = Math.floor(msDifference / msInMonth);
+  const weeks = Math.floor(msDifference / msInWeek);
+  const days = Math.floor(msDifference / msInDay);
+  const hours = Math.floor(msDifference / msInHour);
 
-  const weeks = Math.floor(duration.asWeeks());
-  duration.subtract(weeks, 'weeks');
-
-  const days = Math.floor(duration.asDays());
-  duration.subtract(days, 'days');
-
-  const hours = Math.floor(duration.asHours());
-  duration.subtract(hours, 'hours');
-
-  if (years === 0 && months === 0 && weeks === 0 && hours >= 0) {
+  if (years === 0 && months === 0 && weeks === 0 && days === 0 && hours >= 0) {
     return `${hours === 0 ? '1 h' : `${hours} h`}`;
   }
-
+  if (years === 0 && months === 0 && weeks === 0 && days > 0) {
+    return `${days} d`;
+  }
   if (years === 0 && months === 0 && weeks > 0) {
     return `${weeks} w`;
   }
-
   if (years === 0 && months > 0) {
     return `${months} m`;
   }
-
   if (years > 0) {
     return `${years} y`;
   }
