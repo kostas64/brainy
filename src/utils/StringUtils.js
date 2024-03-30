b97baaa931ca4ad55383390ab402313c75000cfa
Konstantinos Efkarpidis <kostas11062@gmail.com>
2024-03-30 20:30:57 +0200

    new string utils for time format

--- src/utils/StringUtils.js
+++ src/utils/StringUtils.js
@@ -9,22 +9,67 @@ export const getAdaptedScores = (game, scores, passedScores) => {
     const item = passedScores?.filter(gm => gm.game === game)?.[0]?.data?.[0];
     const ms = item?.milliseconds;
     const points = item?.points;
+    const flips = item?.flips;
+    const time = msToMMSSmmm(ms);
+
+    const scoreUnit = flips
+      ? `${flips} flips`
+      : points
+      ? `${points} points`
+      : '';
+
     const scoreLabel = `${`${
-      ms
-        ? `${ms / 1000}s (${item?.flips} flips)`
-        : `${points} points (${item?.correctness}%)`
+      ms ? `${time} (${scoreUnit})` : `${points} points (${item?.correctness}%)`
     }`}`;
+
     const score = !!ms || !!points ? scoreLabel : dict?.gamesNoScore;
     return score;
   } else {
     const ms = scores[game]?.[0]?.milliseconds;
     const points = scores[game]?.[0]?.points;
+    const flips = scores[game]?.[0]?.flips;
+    const time = msToMMSSmmm(ms);
+
+    const scoreUnit = flips
+      ? `${flips} flips`
+      : points
+      ? `${points} points`
+      : '';
+
     const scoreLabel = `${`${
       ms
-        ? `${ms / 1000}s (${scores[game]?.[0]?.flips} flips)`
+        ? `${time} (${scoreUnit})`
         : `${points} points (${scores[game]?.[0]?.correctness}%)`
     }`}`;
     const score = !!ms || !!points ? scoreLabel : dict?.gamesNoScore;
     return score;
   }
+};
+
+export const msToMMSSmmm = milliseconds => {
+  // Calculate minutes
+  var minutes = Math.floor(milliseconds / 60000);
+
+  // Calculate remaining milliseconds after subtracting minutes
+  var remainingMs = milliseconds % 60000;
+
+  // Calculate seconds
+  var seconds = Math.floor(remainingMs / 1000);
+
+  // Calculate remaining milliseconds after subtracting seconds
+  var remainingMs = remainingMs % 1000;
+
+  // Format the result as MM:SS:mmm
+  var result =
+    padZero(minutes) + ':' + padZero(seconds) + ':' + padZero(remainingMs, 3);
+  return result;
+};
+
+// Helper function to pad zeros for formatting
+const padZero = (num, size = 2) => {
+  var numString = num.toString();
+  while (numString.length < size) {
+    numString = '0' + numString;
+  }
+  return numString;
 };
